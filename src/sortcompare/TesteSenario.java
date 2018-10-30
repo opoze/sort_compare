687e4698fc97c37af904905429e4ab9fa4711c83
Luis Alberto Zagnel Pozenato <opoze@hotmail.com>
2018-10-30 11:24:03 -0300

    Inicial READEME incompleto. Gerafor de Senarios incompleto.

---
+++ src/sortcompare/TesteSenario.java
@@ -0,0 +1,15 @@
+package sortcompare;
+
+/**
+ *
+ * @author luispozenato
+ */
+public class TesteSenario {
+
+    public static void main(String[] args) {
+        SenarioGenerator senario = new SenarioGenerator();
+        senario.descPow(10);
+        senario.dump();
+    }
+    
+}
